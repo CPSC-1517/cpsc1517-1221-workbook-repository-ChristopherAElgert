25e1b44994e5d4d81e4f60f950c37b2b7c77fe66
Christopher Elgert <celgert1@nait.ca>
2022-09-16 11:48:11 -0600

    End of Class

---
+++ CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Coach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    //Define a class name Coach that inherits from the base class Person
+    public class Coach : Person 
+    {
+        //Define an auto-implemented property for HireDate
+        public DateTime HireDate { get; set; }
+
+        //Define constructor that passes FullName to the Person base class
+        public Coach(string fullName, DateTime hireDate): base(fullName)
+        {
+            HireDate = hireDate;
+        }
+    }
+
+   
+}
---
+++ CPSC1517-1221-A03-demos/NhlSystem/NhlSystem/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhlSystem
+{
+    //Define a class name Player that inherits from the base class Person
+    public class Player : Person
+    {
+        //Define an auto-implemented property for PrimaryNo
+        public int PrimaryNumber { get; set; }
+        public Position Position { get; set; }
+        public int Assists { get; set; }
+        public int Goals { get; set; }
+
+        public int Points
+        {
+            get
+            {
+                return Goals + Assists;
+            }
+        }
+
+        //Defome a constructor that passes FullName to the Person base class
+        public Player(string fullName, Position position, int primaryNumber) : base(fullName)
+        {
+            PrimaryNumber = primaryNumber;
+            Position = position;
+            Goals = 0;
+            Assists = 0;
+        }
+
+        public Player(string fullName, Position position, int primaryNumber, int goals, int assists) : base(fullName)
+        {
+            PrimaryNumber = primaryNumber;
+            Position = position;
+            Goals = goals;
+            Assists = assists;
+        }
+    }
+}
