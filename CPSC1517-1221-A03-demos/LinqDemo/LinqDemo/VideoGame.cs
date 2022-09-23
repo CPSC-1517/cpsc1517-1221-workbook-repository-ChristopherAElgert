35fd100e6fe72bfc5e6c46db7caf59a1439acd53
Christopher Elgert <celgert1@nait.ca>
2022-09-23 11:14:30 -0600

    LinQ

---
+++ CPSC1517-1221-A03-demos/LinqDemo/LinqDemo/VideoGame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    public record VideoGame(string title, string platform, double price, long webCode);
+
+
+
+}
