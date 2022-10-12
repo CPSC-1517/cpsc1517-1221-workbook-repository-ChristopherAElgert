1e14a32aa2f50a59d2a11cc0e9571aece4d7367c
celgert1 <celgert1@WB304-06>
2022-10-12 16:50:20 -0600

    forms

---
+++ WestwindSolution/WestwindWebApp/Pages/FormControlDemoUsingPureHTML.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WestwindWebApp.Pages
+{
+    public class FormControlDemoUsingPureHTMLModel : PageModel
+    {
+        [TempData]
+        public string FeedbackMessage { get; set; }
+
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public int Age { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            FeedbackMessage = $"Username = {Username}, Age = {Age}";
+        }
+    }
+}
